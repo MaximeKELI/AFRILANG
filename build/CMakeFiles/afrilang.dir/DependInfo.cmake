f0a908c68f1b78066005cb466ab57be9b4b28ac8
MaximeKELI <presidentnetero01@gmail.com>
2026-06-13 00:14:09 +0000

    All coders

--- build/CMakeFiles/afrilang.dir/DependInfo.cmake
+++ build/CMakeFiles/afrilang.dir/DependInfo.cmake
@@ -15,10 +15,12 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/maxime/AFRILANG/src/parser/parser.cpp" "CMakeFiles/afrilang.dir/src/parser/parser.cpp.o" "gcc" "CMakeFiles/afrilang.dir/src/parser/parser.cpp.o.d"
   "/home/maxime/AFRILANG/src/repl/repl.cpp" "CMakeFiles/afrilang.dir/src/repl/repl.cpp.o" "gcc" "CMakeFiles/afrilang.dir/src/repl/repl.cpp.o.d"
   "/home/maxime/AFRILANG/src/semantic/analyzer.cpp" "CMakeFiles/afrilang.dir/src/semantic/analyzer.cpp.o" "gcc" "CMakeFiles/afrilang.dir/src/semantic/analyzer.cpp.o.d"
+  "/home/maxime/AFRILANG/src/serve/server.cpp" "CMakeFiles/afrilang.dir/src/serve/server.cpp.o" "gcc" "CMakeFiles/afrilang.dir/src/serve/server.cpp.o.d"
   "/home/maxime/AFRILANG/src/utils/cli.cpp" "CMakeFiles/afrilang.dir/src/utils/cli.cpp.o" "gcc" "CMakeFiles/afrilang.dir/src/utils/cli.cpp.o.d"
   "/home/maxime/AFRILANG/src/utils/compiler.cpp" "CMakeFiles/afrilang.dir/src/utils/compiler.cpp.o" "gcc" "CMakeFiles/afrilang.dir/src/utils/compiler.cpp.o.d"
   "/home/maxime/AFRILANG/src/utils/diagnostics.cpp" "CMakeFiles/afrilang.dir/src/utils/diagnostics.cpp.o" "gcc" "CMakeFiles/afrilang.dir/src/utils/diagnostics.cpp.o.d"
   "/home/maxime/AFRILANG/src/utils/formatter.cpp" "CMakeFiles/afrilang.dir/src/utils/formatter.cpp.o" "gcc" "CMakeFiles/afrilang.dir/src/utils/formatter.cpp.o.d"
+  "/home/maxime/AFRILANG/src/utils/pkg.cpp" "CMakeFiles/afrilang.dir/src/utils/pkg.cpp.o" "gcc" "CMakeFiles/afrilang.dir/src/utils/pkg.cpp.o.d"
   "/home/maxime/AFRILANG/src/utils/project.cpp" "CMakeFiles/afrilang.dir/src/utils/project.cpp.o" "gcc" "CMakeFiles/afrilang.dir/src/utils/project.cpp.o.d"
   "/home/maxime/AFRILANG/src/utils/stdlib_registry.cpp" "CMakeFiles/afrilang.dir/src/utils/stdlib_registry.cpp.o" "gcc" "CMakeFiles/afrilang.dir/src/utils/stdlib_registry.cpp.o.d"
   )
