2f48b9aa4f593d8d82fa2b47c7a7c0dd5634a487
MaximeKELI <presidentnetero01@gmail.com>
2026-06-13 00:03:20 +0000

    All coders

--- src/codegen/codegen.cpp
+++ src/codegen/codegen.cpp
@@ -223,28 +223,8 @@ void CodeGenerator::emitFunction(std::ostream& out, const FunctionNode& func,
 
     if (ownerClass) {
         bool shouldOverride = false;
-        if (!ownerClass->baseClass.empty()) {
-            auto baseIt = semantic_.classes.find(ownerClass->baseClass);
-            if (baseIt != semantic_.classes.end() &&
-                baseIt->second.methods.count(func.name)) {
-                shouldOverride = true;
-            }
-        }
-        for (const auto& ifaceName : semantic_.classes.at(ownerClass->name).name == ownerClass->name
-                 ? std::vector<std::string>{} : std::vector<std::string>{}) {
-            (void)ifaceName;
-        }
-        (void)shouldOverride;
-        // override if base or interface defines method
         for (const auto& cls : program_.classes) {
             if (cls->name != ownerClass->name) continue;
-            for (const auto& iface : cls->interfaceNames) {
-                auto iit = semantic_.interfaces.find(iface);
-                if (iit != semantic_.interfaces.end() &&
-                    iit->second.methods.count(func.name)) {
-                    shouldOverride = true;
-                }
-            }
             if (!cls->baseClassName.empty()) {
                 auto baseIt = semantic_.classes.find(cls->baseClassName);
                 if (baseIt != semantic_.classes.end() &&
@@ -252,6 +232,14 @@ void CodeGenerator::emitFunction(std::ostream& out, const FunctionNode& func,
                     shouldOverride = true;
                 }
             }
+            for (const auto& iface : cls->interfaceNames) {
+                auto iit = semantic_.interfaces.find(iface);
+                if (iit != semantic_.interfaces.end() &&
+                    iit->second.methods.count(func.name)) {
+                    shouldOverride = true;
+                }
+            }
+            break;
         }
         if (shouldOverride) out << " override";
     }
