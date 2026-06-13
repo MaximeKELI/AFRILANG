4723b786317cbc17cdaaf83a9e1c2cdeeeade074
MaximeKELI <presidentnetero01@gmail.com>
2026-06-13 00:09:22 +0000

    All coders

--- build/fields.generated.cpp
+++ build/fields.generated.cpp
@@ -11,31 +11,31 @@ public:
     double age;
 
     Person(std::string aName, double aAge) {
-        #line 6 "/home/maxime/AFRILANG/examples/fields.afr"
+        #line 6 "/home/maxime/AFRILANG/build/../examples/fields.afr"
         name = aName;
-        #line 7 "/home/maxime/AFRILANG/examples/fields.afr"
+        #line 7 "/home/maxime/AFRILANG/build/../examples/fields.afr"
         age = aAge;
     }
 
     virtual void greet() {
-        #line 11 "/home/maxime/AFRILANG/examples/fields.afr"
+        #line 11 "/home/maxime/AFRILANG/build/../examples/fields.afr"
         std::cout << name << std::endl;
-        #line 12 "/home/maxime/AFRILANG/examples/fields.afr"
+        #line 12 "/home/maxime/AFRILANG/build/../examples/fields.afr"
         std::cout << age << std::endl;
     }
 
 };
 
 int main() {
-    #line 16 "/home/maxime/AFRILANG/examples/fields.afr"
+    #line 16 "/home/maxime/AFRILANG/build/../examples/fields.afr"
     Person person(std::string{}, double{});
-    #line 17 "/home/maxime/AFRILANG/examples/fields.afr"
+    #line 17 "/home/maxime/AFRILANG/build/../examples/fields.afr"
     person.greet();
-    #line 19 "/home/maxime/AFRILANG/examples/fields.afr"
+    #line 19 "/home/maxime/AFRILANG/build/../examples/fields.afr"
     Person bob(std::string{}, double{});
-    #line 20 "/home/maxime/AFRILANG/examples/fields.afr"
+    #line 20 "/home/maxime/AFRILANG/build/../examples/fields.afr"
     bob.name = "Bob";
-    #line 21 "/home/maxime/AFRILANG/examples/fields.afr"
+    #line 21 "/home/maxime/AFRILANG/build/../examples/fields.afr"
     std::cout << bob.name << std::endl;
     return 0;
 }
--- build/modules.generated.cpp
+++ build/modules.generated.cpp
@@ -10,12 +10,12 @@ namespace Math {
 
 namespace Math {
     double add(double a, double b) {
-        #line 3 "/home/maxime/AFRILANG/examples/modules.afr"
+        #line 3 "/home/maxime/AFRILANG/build/../examples/modules.afr"
         return (a + b);
     }
 
     double multiply(double a, double b) {
-        #line 7 "/home/maxime/AFRILANG/examples/modules.afr"
+        #line 7 "/home/maxime/AFRILANG/build/../examples/modules.afr"
         return (a * b);
     }
 
@@ -24,10 +24,10 @@ namespace Math {
 using namespace Math;
 
 int main() {
-    #line 11 "/home/maxime/AFRILANG/examples/modules.afr"
-        #line 13 "/home/maxime/AFRILANG/examples/modules.afr"
+    #line 11 "/home/maxime/AFRILANG/build/../examples/modules.afr"
+        #line 13 "/home/maxime/AFRILANG/build/../examples/modules.afr"
     std::cout << Math::add(3, 4) << std::endl;
-    #line 14 "/home/maxime/AFRILANG/examples/modules.afr"
+    #line 14 "/home/maxime/AFRILANG/build/../examples/modules.afr"
     std::cout << Math::multiply(5, 6) << std::endl;
     return 0;
 }
