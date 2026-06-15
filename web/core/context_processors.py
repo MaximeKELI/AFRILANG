9bb62d9a50f5eaca1fecdeba39e924d4dd445f7f
MaximeKELI <presidentnetero01@gmail.com>
2026-06-15 18:08:00 +0000

    All coders

--- web/core/context_processors.py
+++ web/core/context_processors.py
@@ -5,8 +5,11 @@ def site_settings(request):
     return {
         'AFRILANG_BIN': settings.AFRILANG_BIN,
         'SITE_STATS': {
+            'version': '1.0',
+            'compiler_version': '1.0.0',
             'examples': 60,
             'blessed_packages': 10,
-            'stdlib_modules': 14,
+            'stdlib_modules': 463,
+            'cli_commands': 15,
         },
     }
--- web/core/urls.py
+++ web/core/urls.py
@@ -4,8 +4,17 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('language/', views.language, name='language'),
     path('explore/', views.explore, name='explore'),
-    path('docs/', views.docs, name='docs'),
+    path('docs/', views.docs_overview, name='docs'),
+    path('docs/overview/', views.docs_overview, name='docs_overview'),
+    path('docs/getting-started/', views.docs_getting_started, name='docs_getting_started'),
+    path('docs/syntax/', views.docs_syntax, name='docs_syntax'),
+    path('docs/types/', views.docs_types, name='docs_types'),
+    path('docs/oop/', views.docs_oop, name='docs_oop'),
+    path('docs/advanced/', views.docs_advanced, name='docs_advanced'),
+    path('docs/stdlib/', views.docs_stdlib, name='docs_stdlib'),
+    path('docs/tooling/', views.docs_tooling, name='docs_tooling'),
     path('packages/', views.packages_list, name='packages'),
     path('playground/', views.playground, name='playground'),
     path('api/packages/', views.api_packages, name='api_packages'),
