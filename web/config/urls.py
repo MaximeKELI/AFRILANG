51ea1550587788e472440fe839d9051e303105e5
MaximeKELI <presidentnetero01@gmail.com>
2026-06-15 20:26:24 +0000

    All coders

--- web/config/urls.py
+++ web/config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 from django.views.i18n import set_language
+from django.conf.urls.i18n import i18n_patterns
 
 from core.sitemaps import StaticViewSitemap
 
@@ -12,15 +13,13 @@ sitemaps = {
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/setlang/', set_language, name='set_language'),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap',
+    ),
 ]
-
-urlpatterns += [
-    path('', include('core.urls')),
-]
-
-# English prefix: /en/...
-from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns += i18n_patterns(
     path('', include('core.urls')),
