f8952951198dba70d45097f3b138f2de8d471798
MaximeKELI <presidentnetero01@gmail.com>
2026-06-16 00:06:58 +0000

    All coders

--- examples/conditions.generated.cpp
+++ examples/conditions.generated.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdint>
 #include <unordered_map>
 #include <stdexcept>
 #include <functional>
@@ -11,28 +12,28 @@
 
 int main() {
     #line 1 "/home/maxime/AFRILANG/examples/conditions.afr"
-    double age = 25;
+    double age = 25LL;
     #line 5 "/home/maxime/AFRILANG/examples/conditions.afr"
-    if ((age > 18)) {
+    if ((age > 18LL)) {
         #line 4 "/home/maxime/AFRILANG/examples/conditions.afr"
         std::cout << "Adulte" << std::endl;
     }
     #line 9 "/home/maxime/AFRILANG/examples/conditions.afr"
-    if ((age < 30)) {
+    if ((age < 30LL)) {
         #line 8 "/home/maxime/AFRILANG/examples/conditions.afr"
         std::cout << "Jeune adulte" << std::endl;
     }
     #line 11 "/home/maxime/AFRILANG/examples/conditions.afr"
-    double score = 100;
+    double score = 100LL;
     #line 15 "/home/maxime/AFRILANG/examples/conditions.afr"
-    if ((score == 100)) {
+    if ((score == 100LL)) {
         #line 14 "/home/maxime/AFRILANG/examples/conditions.afr"
         std::cout << "Score parfait!" << std::endl;
     }
     #line 17 "/home/maxime/AFRILANG/examples/conditions.afr"
-    double x = 10;
+    double x = 10LL;
     #line 18 "/home/maxime/AFRILANG/examples/conditions.afr"
-    double y = 3;
+    double y = 3LL;
     #line 20 "/home/maxime/AFRILANG/examples/conditions.afr"
     std::cout << (x + y) << std::endl;
     #line 21 "/home/maxime/AFRILANG/examples/conditions.afr"
@@ -42,7 +43,7 @@ int main() {
     #line 23 "/home/maxime/AFRILANG/examples/conditions.afr"
     std::cout << (x / y) << std::endl;
     #line 27 "/home/maxime/AFRILANG/examples/conditions.afr"
-    for (int _i = 0; _i < static_cast<int>(2); ++_i) {
+    for (int _i = 0; _i < static_cast<int>(2LL); ++_i) {
         #line 26 "/home/maxime/AFRILANG/examples/conditions.afr"
         std::cout << "Boucle conditionnelle" << std::endl;
     }
