a3289be1a960cdd8295a0b544a5b555d35924ceb
MaximeKELI <presidentnetero01@gmail.com>
2026-06-17 13:59:44 +0000

    All coders

---
+++ include/afrilang/version.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef AFRILANG_VERSION
+#define AFRILANG_VERSION "1.0.0"
+#endif
+
+namespace afrilang {
+
+inline constexpr const char* kVersion = AFRILANG_VERSION;
+
+} // namespace afrilang
