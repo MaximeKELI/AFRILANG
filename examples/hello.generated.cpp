8fc4ea446ec58c2b775ec72cf5a2d1340a27b0f6
MaximeKELI <presidentnetero01@gmail.com>
2026-07-19 11:07:00 +0000

    Recuperation de compte

--- examples/hello.generated.cpp
+++ examples/hello.generated.cpp
@@ -4,20 +4,22 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdint>
 #include <unordered_map>
 #include <stdexcept>
 #include <functional>
+#include <optional>
 #include "str.hpp"
 
 int main() {
-    #line 1 "/home/maxime/AFRILANG/examples/hello.afr"
-    std::cout << "Bonjour depuis AFRILANG!" << std::endl;
-    #line 2 "/home/maxime/AFRILANG/examples/hello.afr"
-    std::cout << "Le compilateur fonctionne." << std::endl;
-    #line 6 "/home/maxime/AFRILANG/examples/hello.afr"
-    for (int _i = 0; _i < static_cast<int>(3); ++_i) {
-        #line 5 "/home/maxime/AFRILANG/examples/hello.afr"
-        std::cout << "Hello" << std::endl;
+    #line 1 "/home/admin20/AFRILANG/examples/hello.afr"
+    std::cout << afrilang::runtime::str::toString("Bonjour depuis AFRILANG!") << std::endl;
+    #line 2 "/home/admin20/AFRILANG/examples/hello.afr"
+    std::cout << afrilang::runtime::str::toString("Le compilateur fonctionne.") << std::endl;
+    #line 6 "/home/admin20/AFRILANG/examples/hello.afr"
+    for (int _i = 0; _i < static_cast<int>(3LL); ++_i) {
+        #line 5 "/home/admin20/AFRILANG/examples/hello.afr"
+        std::cout << afrilang::runtime::str::toString("Hello") << std::endl;
     }
     return 0;
 }
